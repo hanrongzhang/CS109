a9d59429e5d28edec56688e21f65a0dd7186d6cf
michaelsuo <darthsuo@gmail.com>
2013-12-07 04:46:44 -0500

    add initial classifier and training set

---
+++ Data/sentiment.py
@@ -0,0 +1,55 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cross_validation import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+import numpy as np
+
+import pandas as pd
+
+# import training data
+training_set = pd.read_csv('training_set.csv')
+
+# take only snippets where the two independent sentiment tags agree
+training_set = training_set[training_set['Agreement'] == 'TRUE']
+
+# sort and tag sets
+tagged = []
+for index, snippet  in training_set.iterrows():
+	if any([snippet['Ann1'] == 'POS', snippet['Ann2'] == 'POS', snippet['Ann3'] == 'POS', snippet['Ann4'] == 'POS']):
+		tagged.append((snippet['Quote'], 'pos'))
+
+	elif any([snippet['Ann1'] == 'NEG',snippet['Ann2'] == 'NEG',snippet['Ann3'] == 'NEG',snippet['Ann4'] == 'NEG']):
+		text_filtered = [s.lower() for s in snippet['Quote'].split() if len(s) > 2]
+		tagged.append((snippet['Quote'], 'neg'))
+
+# feature extraction
+def vectorize_trainer(training_data, vectorizer = None):
+	if vectorizer == None:
+		vec = CountVectorizer(charset_error='ignore', min_df=0)
+	else:
+		vec = vectorizer
+
+	Y = np.empty(len(training_data))
+
+	words = []
+	for index,(quote,sentiment) in enumerate(training_data):
+		words.append(quote)
+		Y[index] = 1 if sentiment == 'pos' else 0
+
+	vec.fit(words)
+
+	bag = vec.transform(words)
+	bag = bag.toarray()
+
+	X = np.array(bag)
+
+	return X,Y
+
+# running classifer on randomly split test/train data
+X,Y = vectorize_trainer(tagged)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y)
+clf = MultinomialNB()
+clf = clf.fit(X_train, Y_train)
+print clf.score(X_test, Y_test)
+print clf.score(X_train, Y_train)
+
+# df = pd.read_csv('all_data.csv', encoding = 'UTF-8')
