fd021a006ad8ce635713b0f4d35d325580d27bc4
dtay42 <omnicronwarlord@gmail.com>
2013-12-06 23:07:13 -0500

    filter v 2

--- aggregate.py
+++ aggregate.py
@@ -1,5 +1,30 @@
 
 #define needed date conversion functions
+from collections import defaultdict
+import json
+import requests
+import warnings
+from pattern import web
+from pattern.web import plaintext
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pprint
+import datetime as datetime
+from datetime import date, timedelta as td
+import string
+import re
+
+from matplotlib import rcParams
+import matplotlib.cm as cm
+import matplotlib as mpl
+
+import random
+import math
+from scipy import stats
+import brewer2mpl
+
 def globe_date_convert(date):
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     temp = date.split(' ')
@@ -29,28 +54,30 @@ def nydn_date_convert(date):
     return date_result
 
 
-
+#clean Chicago Tribune
+chicago_frame = pd.read_csv("./Chicago_Tribune_Data/ChicagoTribune_romney_or_obama_newer.csv", encoding = "UTF-8")
+chicago_frame['date'] = chicago_frame.date.apply(globe_date_convert)
 
 #clean globe data
-globe_frame = pd.read_csv("./Globe_Data/Globe_election_2012.csv", encoding = "UTF-8")
-globe_frame = globe_frame.drop(['section'], 1)
+globe_frame = pd.read_csv("./Globe_Data/Globe_romney_or_obama.csv", encoding = "UTF-8")
 globe_frame['date'] = globe_frame.date.apply(globe_date_convert)
-
-
 
 #clean guardian data
 guardian_frame = pd.read_csv("./Guardian_Data/Guardian_2012_election_full.csv", encoding = "UTF-8")
-guardian_frame = guardian_frame.drop(['id','sectionName','trailText','url'],1)
+guardian_frame = guardian_frame.drop(['id','trailText','url'],1)
 
 guardian_frame['date'] = guardian_frame.pub_date.apply(guardian_date_convert)
 guardian_frame = guardian_frame.drop('pub_date',1)
-guardian_frame.rename(columns={'standfirst': 'abstract'}, inplace=True)
+guardian_frame.rename(columns={'standfirst': 'abstract', 'sectionName':'section'}, inplace=True)
 guardian_frame['source'] = 'Guardian'
 
 #clean LATimes data
-latimes_frame = pd.read_csv("./LATimes_Data/LATimes_election_2012.csv", encoding = "UTF-8")
-latimes_frame = latimes_frame.drop(['section'],1)
+latimes_frame = pd.read_csv("./LATimes_Data/LATimes_romney_or_obama_total.csv", encoding = "UTF-8")
 latimes_frame['date'] = latimes_frame.date.apply(globe_date_convert)
+
+#cleaning Newsday
+newsday_frame = pd.read_csv("./Newsday_Data/newsday_romney_or_obama_newer.csv", encoding = "UTF-8")
+newsday_frame['date'] = newsday_frame.date.apply(globe_date_convert)
 
 #clean nydn data
 nydn_frame = pd.read_csv("./NYDN_Data/NYDN_politics_2008_present.csv", encoding = "UTF-8")
@@ -76,41 +103,40 @@ for row in nyt_frame.iterrows():
         temp_list = temp_list + [row[1][1] + ' ' + row[1][11]]
 nyt_frame['abstract'] = temp_list
 
-nyt_frame = nyt_frame.drop(['blog','pub_date','id','news_desk', 'word_count', 'url','lead_paragraph','seo_headline','snippet'],1)
-
-#clean Washington Post data
-wp_frame = pd.read_csv("./WashingtonPost_Data/WP_election_2012.csv", encoding = "UTF-8")
-wp_frame = wp_frame.drop(['section'],1)
-wp_frame['date'] = wp_frame.date.apply(globe_date_convert)
-
-#clean wsj data
-wsj_frame = pd.read_csv("./WSJ_data/WSJ_subset_2012.csv", encoding = "UTF-8")
-wsj_frame['date'] = wsj_frame.pub_date.apply(nydn_date_convert)
-wsj_frame = wsj_frame.drop(['url','pub_date'],1)
-wsj_frame.rename(columns={'snippet':'abstract'},inplace=True)
-
-wsj_frame = wsj_frame[wsj_frame['date'] > datetime.date(2012,1,1)]
-wsj_frame = wsj_frame[wsj_frame['date'] < datetime.date(2012,11,6)]
-
-#cleaning Newsday
-newsday_frame = pd.read_csv("./Newsday_Data/newsday_romney_or_obama_newer.csv", encoding = "UTF-8")
-newsday_frame = newsday_frame.drop(['section'],1)
-newsday_frame['date'] = newsday_frame.date.apply(globe_date_convert)
+nyt_frame = nyt_frame.drop(['blog','pub_date','id', 'url','lead_paragraph','seo_headline','snippet'],1)
+nyt_frame.rename(columns={'news_desk':'section'},inplace=True)
 
 #cleaning PANewspapers
 pa_frame = pd.read_csv("./PA_newspapers_Data/PA_romney_or_obama_newer.csv", encoding = "UTF-8")
-pa_frame = pa_frame.drop(['section'],1)
 pa_frame['date'] = pa_frame.date.apply(globe_date_convert)
+
+#clean Washington Post data
+wp_frame = pd.read_csv("./WashingtonPost_Data/WP_election_2012.csv", encoding = "UTF-8")
+wp_frame['date'] = wp_frame.date.apply(globe_date_convert)
+
+#clean USAToday
+usa_t_frame = pd.read_csv("./USA_Today_data/USAToday_romney_or_obama.csv", encoding = "UTF-8")
+usa_t_frame['date'] = usa_t_frame.date.apply(globe_date_convert)
+
+#clean wsj data
+wsj_frame = pd.read_csv("./WSJ_data/WSJ_obama_romney_newest.csv",encoding = "UTF-8")
+wsj_frame['date'] = wsj_frame.date.apply(globe_date_convert)
+wsj_frame.rename(columns={'snippet':'abstract'},inplace=True)
+wsj_frame = wsj_frame[wsj_frame['date'] > datetime.date(2012,1,1)]
+wsj_frame = wsj_frame[wsj_frame['date'] < datetime.date(2012,11,6)]
 
 #combine dataframes
 total_frame = globe_frame.append(guardian_frame,ignore_index=True)
+total_frame = total_frame.append(chicago_frame,ignore_index=True)
+total_frame = total_frame.append(usa_t_frame,ignore_index=True)
 total_frame = total_frame.append(latimes_frame,ignore_index=True)
 total_frame = total_frame.append(nydn_frame,ignore_index=True)
 total_frame = total_frame.append(nyt_frame,ignore_index=True)
 total_frame = total_frame.append(wp_frame,ignore_index=True)
 total_frame = total_frame.append(newsday_frame,ignore_index=True)
 total_frame = total_frame.append(pa_frame,ignore_index=True)
-total_frame = total_frame.drop(['Unnamed: 0'],1)
+total_frame = total_frame.drop(['Unnamed: 0','keywords','subjects'],1)
 total_frame['id'] = list(xrange(len(total_frame.index)))
+total_frame['abstract'] = total_frame['abstract'].fillna('-')
 total_frame.to_csv("./all_2012_data.csv", encoding = "UTF-8")
 
--- split_data.py
+++ split_data.py
@@ -1,16 +1,44 @@
+#define needed date conversion functions
+from collections import defaultdict
+import json
+import requests
+import warnings
+from pattern import web
+from pattern.web import plaintext
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pprint
+import datetime as datetime
+from datetime import date, timedelta as td
+import string
+import re
+
+from matplotlib import rcParams
+import matplotlib.cm as cm
+import matplotlib as mpl
+
+import random
+import math
+from scipy import stats
+import brewer2mpl
 total_frame = pd.read_csv("./all_2012_data.csv", encoding = "UTF-8")
 total_frame = total_frame.drop(['Unnamed: 0.1'],1)
 
-romney_frame = total_frame[total_frame['abstract'].isnull()]
-romney_frame = test[test['headline'].str.contains("Romney")]
-temp_frame = total_frame[total_frame['abstract'].notnull()]
-temp_frame = temp_frame[temp_frame['abstract'].str.contains("Romney")]
-romney_frame = romney_frame.append(temp_frame, ignore_index=True)
 
-obama_frame = total_frame[total_frame['abstract'].isnull()]
-obama_frame = test[test['headline'].str.contains("Obama")]
-obama_frame = total_frame[total_frame['abstract'].notnull()]
-obama_frame = temp_frame[temp_frame['abstract'].str.contains("Obama")]
-obama_frame = obama_frame.append(temp_frame, ignore_index=True)
+def pres_filter(frame):
+	total_frame = frame
+	total_frame['candidate'] = ''
+	romney_mask = ((total_frame.headline.str.contains("Romney")) & (~total_frame.headline.str.contains('Obama'))) | ((total_frame.abstract.str.contains("Romney")) & (~total_frame.abstract.str.contains("Obama")) & (~total_frame.headline.str.contains('Obama')))
+	obama_mask = ((total_frame.headline.str.contains("Obama")) & (~total_frame.headline.str.contains('Romney'))) | ((total_frame.abstract.str.contains("Obama")) & (~total_frame.abstract.str.contains("Romney")) & (~total_frame.headline.str.contains('Romney')))
+	total_frame.ix[romney_mask, 'candidate'] = "Romney"
+	total_frame.ix[obama_mask, 'candidate'] = "Obama"
+	return total_frame
 
-print obama_frame
+total_frame = pres_filter(total_frame)
+obama_frame = total_frame[total_frame['candidate'] == 'Obama']
+romney_frame = total_frame[total_frame['candidate'] == 'Romney']
+total_frame.to_csv("./all_2012_data.csv", encoding = "UTF-8")
+romney_frame.to_csv("./romney_data.csv", encoding = "UTF-8")
+obama_frame.to_csv("./obama_data.csv", encoding = "UTF-8")
