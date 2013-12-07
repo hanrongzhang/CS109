9ad19bdfd93a00d988dc595b5c611d8681699c18
michaelsuo <darthsuo@gmail.com>
2013-12-07 07:05:54 -0500

    implement sentiment, update aggregated data csv

--- Data/sentiment.py
+++ Data/sentiment.py
@@ -21,7 +21,10 @@ for index, snippet  in training_set.iterrows():
 		text_filtered = [s.lower() for s in snippet['Quote'].split() if len(s) > 2]
 		tagged.append((snippet['Quote'], 'neg'))
 
-# feature extraction
+# prepare feature extractor
+
+
+
 def vectorize_trainer(training_data, vectorizer = None):
 	if vectorizer == None:
 		vec = CountVectorizer(charset_error='ignore', min_df=0)
@@ -42,14 +45,36 @@ def vectorize_trainer(training_data, vectorizer = None):
 
 	X = np.array(bag)
 
-	return X,Y
+	return X,Y,vec
 
 # running classifer on randomly split test/train data
-X,Y = vectorize_trainer(tagged)
+X,Y,vec = vectorize_trainer(tagged)
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y)
 clf = MultinomialNB()
 clf = clf.fit(X_train, Y_train)
-print clf.score(X_test, Y_test)
-print clf.score(X_train, Y_train)
+# print clf.score(X_test, Y_test)
+# print clf.score(X_train, Y_train)
 
-# df = pd.read_csv('all_data.csv', encoding = 'UTF-8')
+# TODO implement cross-validation and model refinement
+
+def classify_data(data_csv, vectorizer=vec):
+	df = pd.read_csv(data_csv, encoding = 'UTF-8')
+
+	s_list = []
+	neg_list = []
+	pos_list = []
+	for index,item in df.iterrows():
+		bag = vectorizer.transform([item['headline'] + item['abstract']])
+		classified = np.array(bag.toarray())
+		sentiment = clf.predict(classified)
+		probs = clf.predict_proba(classified)
+		s_list.append(sentiment)
+		neg_list.append(probs[0][0])
+		pos_list.append(probs[0][1])
+
+	df['sentiment'] = s_list
+	df['neg'] = neg_list
+	df['pos'] = pos_list
+	return df
+
+classify_data('all_data.csv').drop('Unnamed: 0',1).to_csv("./all_data_sentiment.csv", encoding = "UTF-8")
