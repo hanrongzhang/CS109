2d73c58fa68b2c340cde15995b94c3b8d3820182
michaelsuo <darthsuo@gmail.com>
2013-12-08 17:32:22 -0500

    commit before integrating with pos_analysis.py

--- Data/favorability.py
+++ Data/favorability.py
@@ -2,6 +2,8 @@ import pandas as pd
 import datetime
 
 def time_input_handler(aggregated_data):
+	# reads from CSV and returns a df with 'datetime' column filled with datetime objects into memory
+
 	df = pd.read_csv(aggregated_data)
 
 	time_list = []
@@ -12,7 +14,18 @@ def time_input_handler(aggregated_data):
 	df['datetime'] = time_list
 	return df
 
-def compute_favorability(aggregated_data, time):
+def compute_favorability(data_df, date, candidate):
+	# returns favorability score for candidate on date
 
+	df_prepped = data_df[data_df['datetime'] <= date]
+	df_prepped = df_prepped[data_df['candidate'] == candidate]
 
-compute_favorability("all_data_sentiment.csv", 1)
+	for index, item in df_prepped.iterrows():
+		mod_factor = (date - item['datetime']).days
+		print mod_factor
+
+# tests
+df = time_input_handler("all_data_sentiment.csv")
+test_date =  datetime.datetime.strptime("2011-03-04", "%Y-%m-%d")
+
+compute_favorability(df, test_date, 'Romney')
